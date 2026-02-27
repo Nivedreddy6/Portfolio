862e19efc4e5a7483d927de09a0e17657845a8c4
Nivedreddy6 <117154182+Nivedreddy6@users.noreply.github.com>
2026-02-27 21:32:37 +0530

    Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,10 +16,10 @@ const Header = () => {
             <div className="header-inner">
                 <a href="#home" className="logo">NIVED</a>
                 <nav>
-                    <a href="#about">Init</a>
-                    <a href="#skills">Specs</a>
-                    <a href="#projects">Logs</a>
-                    <a href="#contact">Ping</a>
+                    <a href="#about">About</a>
+                    <a href="#skills">Skills</a>
+                    <a href="#projects">Projects</a>
+                    <a href="#contact">Contact</a>
                 </nav>
             </div>
         </header>
